4c30b49e0235587024a85a7f80a2ebc6e25743eb
Tim Trautman <ttmitmbp@TT-MIT-MBP.monkeybrains.net>
2018-11-30 17:35:25 -0800

    Generate 30 points

--- src/curve_fitting/point_sets.clj
+++ src/curve_fitting/point_sets.clj
@@ -15,14 +15,15 @@
   (let [point-set-ix (:point-set state)
         {x-px-pt :x, y-px-pt :y} px-pt-scales
         indent   (* 0.05 (scales/range-size x-px-pt))
-        indented-scale (scales/linear [0 9]
+        indented-scale (scales/linear [0 29]
                                       [(+ (:range-min x-px-pt) indent)
                                        (- (:range-max x-px-pt) indent)])
-        xs       (map indented-scale (range 0 10))
+        xs       (map indented-scale (range 0 30))
         coeffs   (point-coeffs point-set-ix)
         [ys tr]  (interpreters/infer :procedure model/curve-model
-                                     :inputs [xs]
-                                     :intervention-trace (trace/coefficients-trace coeffs))]
+                                     :inputs [xs])]
+                                     ;:intervention-trace (trace/coefficients-trace coeffs))]
+
 
     ;; (clojure.pprint/pprint tr)
 
